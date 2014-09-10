e1e6cb5466eb62b5b2eebabcae75e0a69c259496
Jessica Tung <jessicatung@Jessicas-MacBook-Pro.local>
2014-09-10 16:23:08 -0700

    Adds functionality so that user can input character name, parse json response to get thumnail path, append that thumnail to DOM

--- public/js/application.js
+++ public/js/application.js
@@ -9,22 +9,22 @@ $(document).ready(function () {
 function bindEvents(){
 	console.log("in bindEvents");
 	var $container = $(".container");
-	$container.on("submit", '.getCharId', getInput);
+	$container.on("submit", '.getChar', getInput);
 }
 
 function getInput(){
 	console.log("in getInput function");
 	event.preventDefault();
-	var characterID = $("input").val();
-	_ajaxClient(characterID);
+	var character = $("input").val();
+	_ajaxClient(character);
 }
 
-function _ajaxClient(characterID){
+function _ajaxClient(character){
 	event.preventDefault();
 	var ajaxRequest = $.ajax({
 		url: '/characters',
 		type: 'GET',
-		data: {char_id: characterID}
+		data: {char_name: character}
 	});
 	ajaxRequest.done(appendName);
 }
@@ -32,7 +32,8 @@ function _ajaxClient(characterID){
 function appendName(response){
 	console.log('it worked');
 	console.log(response);
-	$('.charNames').append(response);
+	// Append link to Thumbnail
+	$('.charNames').append('<img src="' + response + '/portrait_fantastic.jpg">');
 }
 
 
