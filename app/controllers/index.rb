68ee373a9e6e2bfaa9c5b3bf4464d42e77cfb1e2
Jessica Tung <jessicatung@Jessicas-MacBook-Pro.local>
2014-09-09 19:02:40 -0700

    Add new skeleton - fresh and empty

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,26 +4,3 @@ get '/' do
   erb :index
 end
 
-#----------- SESSIONS -----------
-
-get '/sessions/new' do
-  # render sign-in page 
-end
-
-post '/sessions' do
-  # sign-in
-end
-
-delete '/sessions/:id' do
-  # sign-out -- invoked 
-end
-
-#----------- USERS -----------
-
-get '/users/new' do
-  # render sign-up page
-end
-
-post '/users' do
-  # sign-up a new user
-end
