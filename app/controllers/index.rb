e64e3d8fcf5aa7cf6b8de330ea5fc05046cc6c54
Jessica Tung <jessicatung@Jessicas-MacBook-Pro.local>
2014-09-09 22:06:37 -0700

    Add some functions and ajax request to get CharacterId from user via form

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,19 +2,22 @@ require 'httparty'
 require 'json'
 require 'digest/md5'
 
-get '/' do
+get '/' do # experiment
 	ts = "1079438"
 	priv_key = "3b499f11f821c489a4ad0f543337f8f25ac131bd"
 	pub_key = "729621b524d6eaef34c62cd78d76dccc"
 	md5hash = Digest::MD5.hexdigest(ts + priv_key + pub_key)
-	p md5hash
-	# route = "http://gateway.marvel.com/v1/characters/1011334?ts=#{ts}&apikey=#{pub_key}&hash=#{md5hash}"
-	route = "http://gateway.marvel.com/v1/public/characters/1011334?ts=1079438&apikey=729621b524d6eaef34c62cd78d76dccc&hash=fb7d878efc72203153b22f30942cb305"
-	p "* " * 50
-	p route
-	p "* " * 50
+	route = "http://gateway.marvel.com/v1/public/characters?&limit=1&ts=#{ts}&apikey=#{pub_key}&hash=#{md5hash}"
   @marvel_response = HTTParty.get(route)
   p @marvel_response
   erb :index
+end
+
+get '/characters' do
+  p "* " *50
+	p params["char_id"]
+  p "* " *50
+  # now want to do httparty to hit api end point, can either send json obj to js land
+  # and append via jquery. OR can parse here and send to erb.
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,24 @@
+function _ajaxClient(characterID){
+		var ajaxRequest = $.ajax({
+			url: '/characters',
+			type: 'GET',
+			data: {char_id: characterID}
+		});
+		ajaxRequest.done(function(){console.log('it worked')})
+}
+
+function getInput(){
+		event.preventDefault();
+		var characterID = $("input").val();
+		_ajaxClient(characterID);
+}
+
 $(document).ready(function () {
+	console.log("heY@")
+	// $('form').submit(getInput);
+	$(document).on("submit", 'form', getInput);
+
+
 
   // Optional - Use AJAX to send an HTTP DELETE request for the sign-out link
-  
-
 });
